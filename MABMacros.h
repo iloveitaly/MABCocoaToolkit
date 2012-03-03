86f8a8b9f6cddeaf5b68e61d5ccf9894c27a43c6
Michael Bianco <iloveitaly@gmail.com>
2012-03-03 14:18:18 -0500

    Adding reusable macros

---
+++ MABMacros.h
@@ -0,0 +1,22 @@
+#define APP_NAME [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"]
+#define APP_VERSION [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]
+#define OPEN_URL(urlString) [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:urlString]]
+
+// Retrieving preference values
+#define PREF_KEY_VALUE(x) [[[NSUserDefaultsController sharedUserDefaultsController] values] valueForKey:(x)]
+#define PREF_KEY_BOOL(x) [(PREF_KEY_VALUE(x)) boolValue]
+#define PREF_SET_KEY_VALUE(x, y) [[[NSUserDefaultsController sharedUserDefaultsController] values] setValue:(y) forKey:(x)]
+#define PREF_OBSERVE_VALUE(x, y) [[NSUserDefaultsController sharedUserDefaultsController] addObserver:y forKeyPath:x options:NSKeyValueObservingOptionOld context:nil];
+
+/* key, observer, object */
+#define OB_OBSERVE_VALUE(x, y, z) [(z) addObserver:y forKeyPath:x options:NSKeyValueObservingOptionOld context:nil];
+
+#ifdef __OBJC__
+static inline BOOL isEmpty(id thing) {
+    return thing == nil
+	|| ([thing respondsToSelector:@selector(length)]
+        && [(NSData *)thing length] == 0)
+	|| ([thing respondsToSelector:@selector(count)]
+        && [(NSArray *)thing count] == 0);
+}
+#endif
